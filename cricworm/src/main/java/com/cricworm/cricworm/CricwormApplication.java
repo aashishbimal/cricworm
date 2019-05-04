c118848efb889b42da2bbbc0f7c9373159f74c33
aashishbimal <aashishbimal@192.168.0.6>
2019-05-04 19:20:14 +0530

    First Commit

---
+++ cricworm/src/main/java/com/cricworm/cricworm/CricwormApplication.java
@@ -0,0 +1,13 @@
+package com.cricworm.cricworm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricwormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricwormApplication.class, args);
+	}
+
+}
---
+++ cricworm/src/test/java/com/cricworm/cricworm/CricwormApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cricworm.cricworm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CricwormApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
